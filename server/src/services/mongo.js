16eb7baea4beb1e178d9f5b205f070fbd1aaadb7
ArdiRizqiansyah <ardirizqiansyah@gmail.com>
2023-09-25 21:14:25 +0800

    change api using spaceX api

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
-// const MONGO_URL = "mongodb+srv://nasa-api:WWCxnJabwLZ1hGxW@nasacluster.pfmeoya.mongodb.net/nasa?retryWrites=true&w=majority&ssl=true";
-const MONGO_URL = "mongodb://nasa-api:WWCxnJabwLZ1hGxW@ac-v8nvduv-shard-00-00.pfmeoya.mongodb.net:27017,ac-v8nvduv-shard-00-01.pfmeoya.mongodb.net:27017,ac-v8nvduv-shard-00-02.pfmeoya.mongodb.net:27017/nasa?ssl=true&replicaSet=atlas-sfmhif-shard-0&authSource=admin&retryWrites=true&w=majority";
+require("dotenv").config();
+
+// your MongoDB Atlas connection string
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection ready!");
